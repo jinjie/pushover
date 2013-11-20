51d2ea2f6fb1a6d4ae3f18ac9c49a51b4b3b052a
Kong Jin Jie <jinjie@aspiredesigns.com.sg>
2013-11-20 23:44:20 +0800

    Adding some comments

--- libraries/Pushover.php
+++ libraries/Pushover.php
@@ -1,11 +1,29 @@
 <?php
 
+/**
+ * A simple CodeIgniter Pushover Library
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @author		Kong Jin Jie
+ * @link		https://github.com/aspiredesigns/pushover
+ */
+
 class Pushover
 {
 
-	protected $config = array();
-	protected $endpoint = 'https://api.pushover.net/1/';
+	protected $config	= array(); // Variable to store the configuration
 
+	protected $endpoint	= 'https://api.pushover.net/1/'; // The base url of Pushover API
+
+	/**
+	 * Pushover constructor
+	 *
+	 * @access	public
+	 * @param	array
+	 * @return	void
+	 */
+	
 	public function __construct($config = array())
 	{
 		$ci =& get_instance();
@@ -13,11 +31,23 @@ class Pushover
 		// Try to load Phil's curl library
 		$ci->load->spark('curl/1.3.0');
 		
+		// Loads the configuration
 		$ci->load->config('pushover', TRUE);
+		
+		// Store the configuration as array into the variable
 		$this->config = $ci->config->item('pushover');
 		
+		// Override any default configuration
 		$this->initialize($config);
 	}
+	
+	/**
+	 * Initialize the configuration
+	 *
+	 * @access	public
+	 * @param	array
+	 * @return	void
+	 */
 	
 	public function initialize($config = array())
 	{
@@ -29,6 +59,16 @@ class Pushover
 			}
 		}
 	}
+	
+	/**
+	 * Push a message
+	 *
+	 * This method may take a short hand
+	 *
+	 * @access	public
+	 * @param	array
+	 * @param	string
+	 */
 	
 	public function push($params = array(), $title = '')
 	{
@@ -44,13 +84,23 @@ class Pushover
 		return $this->_execute('messages', $params);
 	}
 	
+	/**
+	 * Really send the message to the API
+	 *
+	 * @access	protected
+	 * @param	string
+	 * @param	array
+	 */
+	
 	protected function _execute($uri, $params)
 	{
 		$ci =& get_instance();
 		
+		// Sets the token and user for API validation
 		$params['token'] = $this->config['app_key'];
 		$params['user'] = $this->config['user_key'];
-
+		
+		// Send the POST message to the API
 		$result = $ci->curl->simple_post($this->endpoint . $uri . "." . $this->config['format'], $params);
 		
 		if ($result)
@@ -58,17 +108,22 @@ class Pushover
 			switch (strtolower($this->config['format']))
 			{
 				case 'json' :
+					// If format is json, decode the string to object and return
 					return json_decode($result);
 					break;
 				case 'xml' :
+					// If format is xml, convert it to SimpleXMLElement and return
 					return simplexml_load_string($result);
 					break;
 			}
 		}
 		else
 		{
+			// Fail!
 			return FALSE;
 		}
 	}
 
 }
+
+// End of file
